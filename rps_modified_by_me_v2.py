32c6cadd85a4069f40b6d1b824b140d6506885be
Bladedrinks <bladedrinks@gmail.com>
2019-03-11 11:00:58 +0800

    This is a mini game program of 'Rock! Paper! Scissors!' which I coded using as many newly learned tools as possible.

--- rps_modified_by_me_v2.py
+++ rps_modified_by_me_v2.py
@@ -1,9 +1,23 @@
-# This is a game program named 'Rock! Paper! Scissors!'(abbreviated to 'RPS')
+# This is a mini game program named 'Rock! Paper! Scissors!'(abbreviated to 'RPS')
 #
-# Compared to the first version of this program (rps_modified_by_me_v1), this version offers the user a way to quit the
-# game by answering a question raised by the computer right after each round of the game.
+# Compared to the first version of this program (rps_modified_by_me_v1), this version has several quirks different from
+# the first version:
 #
-# Logically, in this RPS game, we need at least two players: one is a human player and the other is a computer player.
+# (1) It offers the user two play modes: 'Person vs Person' and 'Person vs Computer'.
+#
+# (2) It offers the user a way to quit the game by answering a question raised by the computer right after each set of a
+# single round or each round of the game.
+#
+# (3) The human players can enter their name before playing.
+#
+# (4) The game rule can be set by the user before playing by setting the number 'n' in 'best of n'. 'Best of 3' means
+# whoever is first to win 2 sets of a single round wins the round (By the way, how many rounds you'd like to play
+# depends on the users). 'Best of 5' means whoever is first to win 3 sets of a single round wins the round. The numbers
+# '2' in 'best of 3' and '3' in 'best of 5' are called the 'winning score' which is calculated by the formula:
+#
+#                                     x = (n + 1) / 2
+#
+# x stands for the winning score.
 
 
 from random import randint  # import the 'random' module, otherwise the random.randint(a, b) method will be viewed as
@@ -50,7 +64,7 @@ else:  # p2_id == "c":
 # The game rule should be set before playing.
 # The game rule is basically the common 'best of n' or 'best of n sets' - whoever is first to win half of (n + 1) sets
 # wins the whole round (by the way, you can play as many rounds as you want. The number of rounds is unrestricted).
-# For example, if the user want to play 'best of three', he/she should type '3' at the very beginning
+# For example, if the user wants to play 'best of three', he/she should type '3' at the very beginning
 # and whoever is first to win 2 sets (half of 3 + 1 is 2) wins the whole round/game.
 # (Therefore, the 'winning score' is 2, since (3 + 1) / 2 = 2.)
 # If the user want to play 'best of five', he/she should type '5' at the very beginning
@@ -83,7 +97,7 @@ while True:  # Since the number of rounds is unrestricted, we can keep playing t
     elif p2_wins == winning_score:  # 'p2_wins' must go from 0 wins to the 'winning score' to win a single round.
         print(f"\n       \U0001F389 {p2_name} won Round {round} \U0001F389 \n")  # U0001F389 is for 'Celebration' 🎉
         if stay_or_quit() == "n":
-            print("\nThanks for playing. Bye!")
+            print("\n   \U0001F44B Thanks for playing. Bye! \U0001F44B")  # U0001F44B is for 'Goodbye'👋
             break
         round += 1  # The ordinal number of 'round' is incremented by 1 only when either of the two players wins the
         # previous round.
@@ -116,6 +130,7 @@ while True:  # Since the number of rounds is unrestricted, we can keep playing t
     # Initialize Player 2's play/option (Since Player 2 can be another human player or the computer player,
     # the initialization is more complicated than Player 1):
     if p2_id == "p":  # if Player 2 is a person: (when the user chooses the 'Person vs Person' play mode)
+        print("\n\n       ! ! ! No Cheating ! ! !\n" * 10, end="")
         p2_play = input(f"\n* * * User Tip: Type 'quit' or 'q' to quit the game. * * * \n{p2_name_temp} plays: ")
         if p2_play == "quit" or p2_play == "q":
             print("\n   \U0001F44B Thanks for playing. Bye! \U0001F44B")
@@ -128,15 +143,19 @@ while True:  # Since the number of rounds is unrestricted, we can keep playing t
         p2_play = plays[i]
         print(f"{p2_name_temp} plays: {p2_play}")
 
-    # Any human player plays one of the three valid options:
+    # Below is the conditional logic part:
+    # The first statement right below is to check if the input (play options) is valid or not.
+    # If Player 1 plays one of the three valid plays and, at the same time, Player 2 (under the 'Person vs Person' play
+    # mode) plays one of the three valid plays, we go and check the rest of the conditional part; if not, the 'User Tip'
+    # part is triggered.
     if (p1_play == "rock" or p1_play == "paper" or p1_play == "scissors") \
             and (p2_play == "rock" or p2_play == "paper" or p2_play == "scissors"):
 
-        # (1) Tie/Draw (p1 == p2);
+        # Scenario 1: Tie/Draw (p1 == p2);
         if p2_play == p1_play:
             print(f"\nTie!")
 
-        # (2) Player 2 wins ("Rock crushes scissors", "Paper covers rock" or "Scissors cut paper")
+        # Scenario 2: Player 2 wins ("Rock crushes scissors", "Paper covers rock" or "Scissors cut paper")
         elif p2_play == "rock" and p1_play == "scissors":
             print(f"\n{p2_name} won this set!")
             p2_wins += 1
@@ -147,16 +166,16 @@ while True:  # Since the number of rounds is unrestricted, we can keep playing t
             print(f"\n{p2_name} won this set!")
             p2_wins += 1
 
-        # (3) Player 1 wins ("Rock crushes scissors", "Paper covers rock" or "Scissors cut paper")
+        # Scenario 3: Player 1 wins ("Rock crushes scissors", "Paper covers rock" or "Scissors cut paper")
         else:
             print(f"\n{p1_name} won this set!")
             p1_wins += 1
 
-        set += 1  # The number of 'Round' is incremented by 1 right after the human player enters a valid input (notice:
-        # only 'rock', 'paper' and 'scissors' are valid input) each time. Since invalid input doesn't make a new round,
-        # we have to put the counter increment statement 'r += 1' under the 'if' part, instead of putting it under
-        # the 'else' part.
+        set += 1  # 'set' (the number of sets) is incremented by 1 right after the two players enter the valid inputs
+        # (notice: only 'rock', 'paper' and 'scissors' are valid input) each time. Since invalid input doesn't make a
+        # new round, we have to put the counter increment statement 'set += 1' under the 'if' part, instead of putting
+        # it under the 'else' part.
 
-    # When human player plays gibberish (including nothing entered):
+    # When either of the human players plays gibberish (including nothing entered), the control of the program goes here
     else:
         print("\n* * * User Tip: Only 'rock', 'paper' and 'scissors' are valid plays. Check your spelling. * * * ")
