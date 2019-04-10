f4018c2728147a147ab35a558d78612ab1f47f15
Blade <30589245+Bladedrinks@users.noreply.github.com>
2019-04-10 16:01:10 +0800

    Update listcomp_of_the_sieve_of_Eratosthenes_02.py
    Blade added a word "Program" next to "Test" in Line 66.

--- listcomp_of_the_sieve_of_Eratosthenes_02.py
+++ listcomp_of_the_sieve_of_Eratosthenes_02.py
@@ -63,7 +63,7 @@ for i in range(2, n+1):  # Why the range starts at 2 (not 0)? Because 1 is neith
 print(f"There are {len(primes)} primes less than or equal to {n}, which are: \n{primes}")
 
 
-# ====== Test ======
+# ====== Program Test ======
 # Prime numbers under 200 are: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
 # 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199.
 test_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
