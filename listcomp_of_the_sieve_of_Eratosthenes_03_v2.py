88c1dba579392230c4b4de83ce49db54e3105ce6
Blade <30589245+Bladedrinks@users.noreply.github.com>
2019-04-11 13:01:55 +0800

    Update listcomp_of_the_sieve_of_Eratosthenes_03_v2.py
    Just added a word "Program" in Line 26.

--- listcomp_of_the_sieve_of_Eratosthenes_03_v2.py
+++ listcomp_of_the_sieve_of_Eratosthenes_03_v2.py
@@ -23,7 +23,7 @@ prime_ls = [i for i in range(2, n+1) if i not in composite_set]
 print(f"There are {len(prime_ls)} primes less than or equal to {n}, which are: \n{prime_ls}")
 
 
-# ====== Test ======
+# ====== Program Test ======
 # Prime numbers under 200 are: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
 # 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199.
 test_ls = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
